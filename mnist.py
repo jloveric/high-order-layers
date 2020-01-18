0da4d9d105f2a095529989d1bae6019b5357451c
John Loverich <john.loverich@gmail.com>
2020-01-18 08:53:45 -0800

    Updating mnist example.

--- mnist.py
+++ mnist.py
@@ -7,32 +7,18 @@ x_train, x_test = (x_train / 128.0-1.0), (x_test / 128.0-1.0)
 
 units = 128
 
+basis = poly.b3
+
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  poly.Polynomial(units, 28*28),
-  #tf.keras.layers.ReLU(512),
+  poly.Polynomial(units, 28*28, basis=basis),
   tf.keras.layers.LayerNormalization(),
   tf.keras.layers.Dropout(0.2),
-  poly.Polynomial(units, units),
-  #tf.keras.layers.ReLU(512),
+  poly.Polynomial(units, units, basis=basis),
   tf.keras.layers.LayerNormalization(),
   tf.keras.layers.Dropout(0.2),
-  #tf.keras.layers.Dense(512, activation='relu'),
-  #tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation='softmax')
 ])
-
-
-'''
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  #poly.Polynomial(512, 28*28),
-  #tf.keras.layers.ReLU(512),
-  tf.keras.layers.Dense(512, activation='relu'),
-  #tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
-'''
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
