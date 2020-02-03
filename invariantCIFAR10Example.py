3a75cedbc323d755237d527c023cc738d53fd1a8
John Loverich <john.loverich@gmail.com>
2020-02-03 15:07:05 -0800

    Making resnet the standard for invariant problems.

--- invariantCIFAR10Example.py
+++ invariantCIFAR10Example.py
@@ -7,9 +7,8 @@ cifar10 = tf.keras.datasets.cifar10
 x_train, x_test = (x_train / 128.0 - 1.0), (x_test / 128.0 - 1.0)
 
 units = 60
-
 basis = poly.b2C
-
+'''
 model = tf.keras.models.Sequential([
     Flatten(input_shape=(32, 32, 3)),
     poly.Polynomial(units, basis=basis),
@@ -22,6 +21,31 @@ model = tf.keras.models.Sequential([
     LayerNormalization(),
     Dense(10, activation='softmax')
 ])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5, batch_size=10)
+model.evaluate(x_test, y_test)
+'''
+
+#Residual layer
+def res_block(input_data, units=units, basis=basis) :
+    x0 = LayerNormalization()(input_data)
+    x1 = poly.Polynomial(units, basis=basis)(x0)
+    x1 = Add()([x1, input_data])
+    return x1
+
+inputs = tf.keras.Input(shape=(32,32,3))
+x = Flatten(input_shape=(32, 32, 3))(inputs)
+x = poly.Polynomial(units, basis=basis)(x)
+for i in range(3) :
+    x = res_block(x, basis=basis, units=units)
+x = LayerNormalization()(x)
+outputs = Dense(10, activation='softmax')(x)
+
+model = tf.keras.Model(inputs, outputs)
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
--- invariantMnistExample.py
+++ invariantMnistExample.py
@@ -1,3 +1,6 @@
+#Example using polynomial layer in a residual network.  Not particularly great
+#on this problem, but it may do better on time series.
+
 import tensorflow as tf
 from snovalleyai_piecewise_polynomial_layers import PolynomialLayers as poly
 from tensorflow.keras.layers import *
@@ -7,26 +10,29 @@ layers = tf.keras.layers
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = (x_train / 128.0 - 1.0), (x_test / 128.0 - 1.0)
 
-units = 20
+units = 10
+basis = poly.b3
 
-basis = poly.b2C
+#Residual layer
+def res_block(input_data, units=units, basis=basis) :
+    x0 = LayerNormalization()(input_data)
+    x1 = poly.Polynomial(units, basis=basis)(x0)
+    x1 = Add()([x1, input_data])
+    return x1
 
-model = tf.keras.models.Sequential([
-    Flatten(input_shape=(28, 28)),
-    poly.Polynomial(units, basis=basis),
-    LayerNormalization(),
-    poly.Polynomial(units, basis=basis),
-    LayerNormalization(),
-    poly.Polynomial(units, basis=basis),
-    LayerNormalization(),
-    poly.Polynomial(units, basis=basis),
-    LayerNormalization(),
-    Dense(10, activation='softmax')
-])
+inputs = tf.keras.Input(shape=(28,28))
+x = Flatten(input_shape=(28, 28))(inputs)
+x = poly.Polynomial(units, basis=basis)(x)
+for i in range(3) :
+    x = res_block(x, basis=basis, units=units)
+x = LayerNormalization()(x)
+outputs = Dense(10, activation='softmax')(x)
+
+model = tf.keras.Model(inputs, outputs)
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=20, batch_size=10)
+model.fit(x_train, y_train, epochs=5, batch_size=10)
 model.evaluate(x_test, y_test)
